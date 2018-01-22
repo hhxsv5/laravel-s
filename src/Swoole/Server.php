407218d4e75784205f914d2d13643caf44669064
XieBiao <xiebiao@medlinker.com>
2018-01-22 22:49:43 +0800

    fix set process title exclude OS_X

--- src/Swoole/Server.php
+++ src/Swoole/Server.php
@@ -100,6 +100,9 @@ class Server
 
     protected function setProcessTitle($title)
     {
+        if (PHP_OS == 'Darwin') {
+            return;
+        }
         if (function_exists('cli_set_process_title')) {
             cli_set_process_title($title);
         } elseif (function_exists('swoole_set_process_name')) {
