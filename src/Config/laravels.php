8b31869b2e55923784e93379dab146706922c866
XieBiao <xiebiao@medlinker.com>
2018-01-23 22:12:01 +0800

    no necessary to use env() for config

--- src/Config/laravels.php
+++ src/Config/laravels.php
@@ -5,10 +5,10 @@ return [
     'enable_gzip' => extension_loaded('zlib') && env('LARAVELS_ENABLE_GZIP', 1),
     'server'      => env('LARAVELS_SERVER', 'LaravelS'),
     'swoole'      => [
-        'pid_file'      => env('LARAVELS_PID_FILE', storage_path('laravels.pid')),
         'dispatch_mode' => 2,
-        'max_request'   => env('LARAVELS_MAX_REQUEST', 2000),
-        'daemonize'     => env('LARAVELS_DAEMONIZE', 1),
+        'max_request'   => 3000,
+        'daemonize'     => 1,
+        'pid_file'      => storage_path('laravels.pid'),
         'log_file'      => storage_path('logs/swoole-' . date('Y-m-d') . '.log'),
         'log_level'     => 4,
     ],
