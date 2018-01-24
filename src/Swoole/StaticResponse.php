85aa1b6ce8782165e1d63b6c1f840bc86bc80df8
XieBiao <xiebiao@medlinker.com>
2018-01-24 18:06:53 +0800

    file member

--- src/Swoole/StaticResponse.php
+++ src/Swoole/StaticResponse.php
@@ -2,13 +2,22 @@
 
 namespace Hhxsv5\LaravelS\Swoole;
 
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
 class StaticResponse extends Response
 {
+    protected $file;
+
+    public function __construct(\swoole_http_response $swooleResponse, SymfonyResponse $laravelResponse)
+    {
+        parent::__construct($swooleResponse, $laravelResponse);
+        $this->file = $this->laravelResponse->getFile();
+    }
+
     public function sendContent()
     {
-        $file = $this->laravelResponse->getFile();
-        if (filesize($file) > 0) {
-            $this->swooleResponse->sendfile($file);
+        if (filesize($this->file) > 0) {
+            $this->swooleResponse->sendfile($this->file);
         } else {
             $this->swooleResponse->end('');
         }
