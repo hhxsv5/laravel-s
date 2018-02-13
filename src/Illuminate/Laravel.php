e09ff16fa92fbdce0ef765f7be6256ed6f40db30
XieBiao <hhxsv5@sina.com>
2018-02-13 15:12:32 +0800

    create request from globals

--- src/Illuminate/Laravel.php
+++ src/Illuminate/Laravel.php
@@ -63,9 +63,6 @@ class Laravel
 
     protected function setLaravel()
     {
-        // Enables support for the _method request parameter to determine the intended HTTP method.
-        Request::enableHttpMethodParameterOverride();
-
         // Load configuration laravel.php manually for Lumen
         if ($this->conf['isLumen'] && file_exists($this->conf['rootPath'] . '/config/laravels.php')) {
             $this->app->configure('laravels');
--- src/Swoole/Request.php
+++ src/Swoole/Request.php
@@ -3,7 +3,6 @@
 namespace Hhxsv5\LaravelS\Swoole;
 
 use Illuminate\Http\Request as IlluminateRequest;
-use Symfony\Component\HttpFoundation\HeaderBag;
 
 class Request
 {
@@ -16,24 +15,29 @@ class Request
 
     public function toIlluminateRequest()
     {
-        $get = isset($this->swooleRequest->get) ? $this->swooleRequest->get : [];
-        $post = isset($this->swooleRequest->post) ? $this->swooleRequest->post : [];
-        $cookies = isset($this->swooleRequest->cookie) ? $this->swooleRequest->cookie : [];
-        $server = isset($this->swooleRequest->server) ? $this->swooleRequest->server : [];
+        $_GET = isset($this->swooleRequest->get) ? $this->swooleRequest->get : [];
+        $_POST = isset($this->swooleRequest->post) ? $this->swooleRequest->post : [];
+        $_COOKIE = isset($this->swooleRequest->cookie) ? $this->swooleRequest->cookie : [];
+        $_SERVER = isset($this->swooleRequest->server) ? $this->swooleRequest->server : [];
         $headers = isset($this->swooleRequest->header) ? $this->swooleRequest->header : [];
-        $files = isset($this->swooleRequest->files) ? $this->swooleRequest->files : [];
+        $_FILES = isset($this->swooleRequest->files) ? $this->swooleRequest->files : [];
+        $_ENV = [];
+        $_REQUEST = [];
 
         foreach ($headers as $key => $value) {
             $key = str_replace('-', '_', $key);
-            $server['http_' . $key] = $value;
+            $_SERVER['http_' . $key] = $value;
         }
-        $server = array_change_key_case($server, CASE_UPPER);
+        $_SERVER = array_change_key_case($_SERVER, CASE_UPPER);
 
-        $content = $this->swooleRequest->rawContent() ?: null;
-        $laravelRequest = new IlluminateRequest($get, $post, [], $cookies, $files, $server, $content);
-        $laravelRequest->headers = new HeaderBag($headers);
+        $request = ['C' => $_COOKIE, 'G' => $_GET, 'P' => $_POST];
+        $requestOrder = ini_get('request_order') ?: ini_get('variables_order');
+        $requestOrder = preg_replace('#[^CGP]#', '', strtoupper($requestOrder)) ?: 'GP';
+        foreach (str_split($requestOrder) as $order) {
+            $_REQUEST = array_merge($_REQUEST, $request[$order]);
+        }
 
-        return $laravelRequest;
+        return IlluminateRequest::capture();
     }
 
 }
