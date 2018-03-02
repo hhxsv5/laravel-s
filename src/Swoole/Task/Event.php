35fa2b5489bf379ac186bc0ed94754729548378d
XieBiao <hhxsv5@sina.com>
2018-03-02 09:55:33 +0800

    fire task event

--- src/Swoole/Task/Event.php
+++ src/Swoole/Task/Event.php
@@ -7,4 +7,10 @@ use Illuminate\Queue\SerializesModels;
 abstract class Event
 {
     use SerializesModels;
+
+    public static function fire(self $event)
+    {
+        $taskId = app('swoole')->task($event);
+        return $taskId !== false;
+    }
 }
