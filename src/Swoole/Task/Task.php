a502aa5cfd791eb943957e7a48abb1c3c776f1e4
XieBiao <hhxsv5@sina.com>
2018-03-07 18:03:17 +0800

    delay task

--- src/Swoole/Task/Task.php
+++ src/Swoole/Task/Task.php
@@ -8,15 +8,49 @@ abstract class Task
 {
     use SerializesModels;
 
+    /**
+     * The number of seconds before the task should be delayed.
+     *
+     * @var int|null
+     */
+    protected $delay;
+
+
+    public function delay($delay)
+    {
+        if ($delay <= 0) {
+            throw new \InvalidArgumentException('The delay must be greater than 0');
+        }
+        if ($delay >= 86400) {
+            throw new \InvalidArgumentException('The max delay is 86400s');
+        }
+        $this->delay = $delay;
+        return $this;
+    }
+
+    public function getDelay()
+    {
+        return $this->delay;
+    }
+
+
     abstract public function handle();
 
     public static function deliver(self $task)
     {
-        try {
-            $taskId = app('swoole')->task($task);
-            return $taskId !== false;
-        } catch (\Exception $e) {
-            return false;
+        $deliver = function () use ($task) {
+            try {
+                $taskId = app('swoole')->task($task);
+                return $taskId !== false;
+            } catch (\Exception $e) {
+                return false;
+            }
+        };
+        if ($task->delay > 0) {
+            swoole_timer_after($task->delay * 1000, $deliver);
+            return true;
+        } else {
+            return $deliver();
         }
     }
 }
