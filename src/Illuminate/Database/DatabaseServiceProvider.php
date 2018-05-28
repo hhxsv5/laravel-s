5f5c817b648c3462f78d8d3eff74a784b35e3882
XieBiao <hhxsv5@sina.com>
2018-05-29 00:08:45 +0800

    re-register db provider
    Signed-off-by: XieBiao <hhxsv5@sina.com>

--- src/Illuminate/Database/DatabaseServiceProvider.php
+++ src/Illuminate/Database/DatabaseServiceProvider.php
@@ -8,16 +8,16 @@ class DatabaseServiceProvider extends IlluminateDatabaseServiceProvider
 {
     public function register()
     {
-//        $this->app->singleton('db.factory', function ($app) {
-//            return new ConnectionFactory($app);
-//        });
-//
-//        $this->app->singleton('db', function ($app) {
-//            return new DatabaseManager($app, $app['db.factory']);
-//        });
-//
-//        $this->app->bind('db.connection', function ($app) {
-//            return $app['db']->connection();
-//        });
+        $this->app->singleton('db.factory', function ($app) {
+            return new ConnectionFactory($app);
+        });
+
+        $this->app->singleton('db', function ($app) {
+            return new DatabaseManager($app, $app['db.factory']);
+        });
+
+        $this->app->bind('db.connection', function ($app) {
+            return $app['db']->connection();
+        });
     }
 }
