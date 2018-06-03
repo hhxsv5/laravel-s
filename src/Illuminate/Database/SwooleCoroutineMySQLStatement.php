f01d58b25f21d61fced119927996f7bdb04825f7
XieBiao <hhxsv5@sina.com>
2018-06-03 21:03:10 +0800

    make swoole mysql client like pdo
    Signed-off-by: XieBiao <hhxsv5@sina.com>

--- src/Illuminate/Database/SwooleCoroutineMySQLStatement.php
+++ src/Illuminate/Database/SwooleCoroutineMySQLStatement.php
@@ -19,9 +19,19 @@ class SwooleCoroutineMySQLStatement
         return $this->statement->affected_rows;
     }
 
+    /**
+     * @param array $params
+     * @param int $timeout
+     * @return bool
+     * @throws StatementException
+     */
     public function execute(array $params = [], $timeout = -1)
     {
         $this->result = $this->statement->execute($params, $timeout);
+        if ($this->statement->errno != 0) {
+            throw new StatementException($this->statement->error, $this->statement->errno);
+        }
+        return true;
     }
 
     public function fetchAll()
