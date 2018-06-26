545a6dfa42bdb39f25ba340343f61d5a9db74103
XieBiao <hhxsv5@sina.com>
2018-06-26 14:32:13 +0800

    compatible with no swoole environment

--- config/laravels.php
+++ config/laravels.php
@@ -6,7 +6,7 @@
 return [
     'listen_ip'          => env('LARAVELS_LISTEN_IP', '127.0.0.1'),
     'listen_port'        => env('LARAVELS_LISTEN_PORT', 5200),
-    'socket_type'        => env('LARAVELS_SOCKET_TYPE', \SWOOLE_SOCK_TCP),
+    'socket_type'        => env('LARAVELS_SOCKET_TYPE', defined('SWOOLE_SOCK_TCP') ? \SWOOLE_SOCK_TCP : 1),
     'enable_gzip'        => env('LARAVELS_ENABLE_GZIP', false),
     'server'             => env('LARAVELS_SERVER', 'LaravelS'),
     'handle_static'      => env('LARAVELS_HANDLE_STATIC', false),
