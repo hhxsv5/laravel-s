38a8e9a76f62f95f9bec23bb5f7dc5e2a7e28396
XieBiao <hhxsv5@sina.com>
2018-06-27 23:30:34 +0800

    Revert "fix file response with prepare by request"
    This reverts commit c08cf017bacd86a01fa09da53537e3e2b283e109.

--- src/Illuminate/Laravel.php
+++ src/Illuminate/Laravel.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request as IlluminateRequest;
 use Illuminate\Contracts\Http\Kernel as HttpKernel;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Hhxsv5\LaravelS\Illuminate\Database\DatabaseServiceProvider;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Laravel
@@ -205,13 +206,13 @@ class Laravel
         $publicPath = $this->conf['static_path'];
         $requestFile = $publicPath . $uri;
         if (is_file($requestFile)) {
-            return $this->createStaticResponse($requestFile, $request);
+            return $this->createStaticResponse($requestFile, $request->header('if-modified-since'));
         } elseif (is_dir($requestFile)) {
             $indexFile = $this->lookupIndex($requestFile);
             if ($indexFile === false) {
                 return false;
             } else {
-                return $this->createStaticResponse($indexFile, $request);
+                return $this->createStaticResponse($indexFile, $request->header('if-modified-since'));
             }
         } else {
             return false;
@@ -230,12 +231,24 @@ class Laravel
         return false;
     }
 
-    public function createStaticResponse($requestFile, IlluminateRequest $request)
+    public function createStaticResponse($requestFile, $modifiedSince = null)
     {
-        $response = new BinaryFileResponse($requestFile);
-        $response->prepare($request);
-        $response->isNotModified($request);
-        return $response;
+        $code = SymfonyResponse::HTTP_OK;
+        $mtime = filemtime($requestFile);
+        if ($modifiedSince !== null) {
+            $modifiedSince = strtotime($modifiedSince);
+            if ($modifiedSince !== false && $modifiedSince >= $mtime) {
+                $code = SymfonyResponse::HTTP_NOT_MODIFIED;
+            }
+        }
+
+        $maxAge = 24 * 3600;
+        $rsp = new BinaryFileResponse($requestFile, $code);
+        $rsp->setLastModified(new \DateTime(date('Y-m-d H:i:s', $mtime)));
+        $rsp->setMaxAge($maxAge);
+        $rsp->setPrivate();
+        $rsp->setExpires(new \DateTime(date('Y-m-d H:i:s', time() + $maxAge)));
+        return $rsp;
     }
 
     public function reRegisterServiceProvider($providerCls, array $clearFacades = [], $force = false)
