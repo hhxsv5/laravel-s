d023ff2618383ae7cf5ca245846b2c0defa3b227
XieBiao <hhxsv5@sina.com>
2018-07-02 23:13:33 +0800

    more swoole pdo methods
    Signed-off-by: XieBiao <hhxsv5@sina.com>

--- src/Illuminate/Database/SwoolePDO.php
+++ src/Illuminate/Database/SwoolePDO.php
@@ -7,65 +7,104 @@ use Swoole\Coroutine\MySQL as SwooleMySQL;
 
 class SwoolePDO extends \PDO
 {
-    protected $coMySQL;
+    protected $sm;
+    protected $isInTransaction = false;
 
     public function __construct()
     {
-        $this->coMySQL = new SwooleMySQL();
+        $this->sm = new SwooleMySQL();
     }
 
     public function connect(array $serverInfo)
     {
-        $this->coMySQL->connect($serverInfo);
+        $this->sm->connect($serverInfo);
     }
 
     public function prepare($statement, $options = null)
     {
-        $oldStatement = $this->coMySQL->prepare($statement);
+        $oldStatement = $this->sm->prepare($statement);
         if ($oldStatement === false) {
-            throw new QueryException($statement, [], new \Exception($this->coMySQL->error, $this->coMySQL->errno));
+            throw new QueryException($statement, [], new \Exception($this->sm->error, $this->sm->errno));
         }
         return new SwoolePDOStatement($oldStatement);
     }
 
     public function beginTransaction()
     {
-        return $this->coMySQL->begin();
+        $this->isInTransaction = true;
+        $this->sm->begin();
     }
 
     public function commit()
     {
-        return $this->coMySQL->commit();
+        $this->sm->commit();
+        $this->isInTransaction = false;
     }
 
     public function rollBack()
     {
-        return $this->coMySQL->rollback();
+        $this->sm->rollback();
+        $this->isInTransaction = false;
     }
 
     public function query($statement, $mode = \PDO::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = [])
     {
-        return $this->coMySQL->query($statement, array_get($ctorargs, 'timeout', 0.0));
+        return $this->sm->query($statement, array_get($ctorargs, 'timeout', 0.0));
     }
 
     public function exec($statement)
     {
-        return $this->coMySQL->query($statement);
+        return $this->sm->query($statement);
     }
 
     public function lastInsertId($name = null)
     {
-        return $this->coMySQL->insert_id;
+        return $this->sm->insert_id;
     }
 
     public function rowCount()
     {
-        return $this->coMySQL->affected_rows;
+        return $this->sm->affected_rows;
     }
 
     public function quote($string, $parameter_type = \PDO::PARAM_STR)
     {
         //TODO
         return $string;
+    }
+
+    public function errorCode()
+    {
+        return $this->sm->errno;
+    }
+
+    public function errorInfo()
+    {
+        return [
+            $this->sm->errno,
+            $this->sm->errno,
+            $this->sm->error,
+        ];
+    }
+
+    public function inTransaction()
+    {
+        return $this->isInTransaction;
+    }
+
+    public function getAttribute($attribute)
+    {
+        return isset($this->sm->serverInfo[$attribute]) ? $this->sm->serverInfo[$attribute] : null;
+    }
+
+    public function setAttribute($attribute, $value)
+    {
+        // TODO
+        return false;
+    }
+
+    public static function getAvailableDrivers()
+    {
+        return ['mysql'];
     }
 }
