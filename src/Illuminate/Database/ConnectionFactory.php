390599c3447d84e022e4fe297a5c81ac00872743
XieBiao <hhxsv5@sina.com>
2018-07-09 12:03:12 +0800

    Fixed compatible with createConnection

--- src/Illuminate/Database/ConnectionFactory.php
+++ src/Illuminate/Database/ConnectionFactory.php
@@ -13,6 +13,10 @@ class ConnectionFactory extends IlluminateConnectionFactory
             throw new \InvalidArgumentException('A driver must be specified.');
         }
 
+        if ($this->container->bound($key = "db.connector.{$config['driver']}")) {
+            return $this->container->make($key);
+        }
+
         switch ($config['driver']) {
             case 'sw-co-mysql':
                 return new CoroutineMySQLConnector();
@@ -20,8 +24,17 @@ class ConnectionFactory extends IlluminateConnectionFactory
         return parent::createConnector($config);
     }
 
-    protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
+    protected function createSingleConnection(array $config)
     {
+        $swoolePdo = $this->createConnector($config)->connect($config);
+        return $this->createSwooleConnection($config['driver'], $swoolePdo, $config['database'], $config['prefix'], $config);
+    }
+
+    protected function createSwooleConnection($driver, SwoolePDO $connection, $database, $prefix = '', array $config = [])
+    {
+        if ($this->container->bound($key = "db.connection.{$driver}")) {
+            return $this->container->make($key, [$connection, $database, $prefix, $config]);
+        }
         switch ($driver) {
             case 'sw-co-mysql':
                 return new SwooleMySQLConnection($connection, $database, $prefix, $config);
