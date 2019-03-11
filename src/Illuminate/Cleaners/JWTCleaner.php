c1d92843e1ea812df3c9e1b3b1bfe0cbdbd04712
XieBiao <hhxsv5@sina.com>
2019-03-11 15:24:17 +0800

    folding jwt instances

--- src/Illuminate/Cleaners/JWTCleaner.php
+++ src/Illuminate/Cleaners/JWTCleaner.php
@@ -7,15 +7,18 @@ use Illuminate\Support\Facades\Facade;
 
 class JWTCleaner implements CleanerInterface
 {
+    protected $instances = [
+        'tymon.jwt',
+        'tymon.jwt.auth',
+        'tymon.jwt.parser',
+        'tymon.jwt.claim.factory',
+    ];
+
     public function clean(Container $app)
     {
-        $app->forgetInstance('tymon.jwt');
-        $app->forgetInstance('tymon.jwt.auth');
-        $app->forgetInstance('tymon.jwt.parser');
-        $app->forgetInstance('tymon.jwt.claim.factory');
-        Facade::clearResolvedInstance('tymon.jwt');
-        Facade::clearResolvedInstance('tymon.jwt.auth');
-        Facade::clearResolvedInstance('tymon.jwt.parser');
-        Facade::clearResolvedInstance('tymon.jwt.claim.factory');
+        foreach ($this->instances as $instance) {
+            $app->forgetInstance($instance);
+            Facade::clearResolvedInstance($instance);
+        }
     }
 }
