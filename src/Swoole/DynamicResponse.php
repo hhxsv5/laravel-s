103986a614e3112fce0c88b8cc52691e745baa19
谢彪 <hhxsv5@sina.com>
2019-06-13 17:10:06 +0800

    support streamed response

--- src/Swoole/DynamicResponse.php
+++ src/Swoole/DynamicResponse.php
@@ -2,6 +2,8 @@
 
 namespace Hhxsv5\LaravelS\Swoole;
 
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 class DynamicResponse extends Response
 {
     const CHUNK_LIMIT = 2097152; // 2M
@@ -20,7 +22,14 @@ class DynamicResponse extends Response
 
     public function sendContent()
     {
-        $content = $this->laravelResponse->getContent();
+        if ($this->laravelResponse instanceof StreamedResponse) {
+            $reflect = new \ReflectionClass(StreamedResponse::class);
+            $contentProperty = $reflect->getProperty('content');
+            $contentProperty->setAccessible(true);
+            $content = $contentProperty->getValue($this->laravelResponse);
+        } else {
+            $content = $this->laravelResponse->getContent();
+        }
 
         $len = strlen($content);
         if ($len === 0) {
