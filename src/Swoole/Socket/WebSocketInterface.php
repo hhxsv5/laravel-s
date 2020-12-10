9d70339c1283b7a8b00419426afee91d1329ae52
Louis <louisni9507@gmail.com>
2020-12-10 15:17:54 +0800

    add onHandshake comment

--- src/Swoole/Socket/WebSocketInterface.php
+++ src/Swoole/Socket/WebSocketInterface.php
@@ -13,4 +13,8 @@ interface WebSocketInterface
     public function onMessage(Server $server, Frame $frame);
 
     public function onClose(Server $server, $fd, $reactorId);
+
+    // It would be conflicted with 'onOpen' if you implement this method as you need to handle 'onOpen' inside this method
+    // Our decision is to make it to be an optional case
+    //public function onHandShake(Request $request,Response $response);
 }
