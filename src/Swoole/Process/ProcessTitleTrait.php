44b4369e739ad9edd7751211292fc75ff64a0f39
hhxsv5 <hhxsv5@sina.com>
2023-04-26 19:21:06 +0800

    avoid cli_set_process_title error

--- src/Swoole/Process/ProcessTitleTrait.php
+++ src/Swoole/Process/ProcessTitleTrait.php
@@ -10,9 +10,9 @@ trait ProcessTitleTrait
             return;
         }
         if (function_exists('cli_set_process_title')) {
-            cli_set_process_title($title);
+            @cli_set_process_title($title);
         } elseif (function_exists('swoole_set_process_name')) {
-            swoole_set_process_name($title);
+            @swoole_set_process_name($title);
         }
     }
 }
